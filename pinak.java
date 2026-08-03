94c05c82a5951619a594085e1162cfdfc4587781
pinakkk <pinakkundu1080@gmail.com>
2026-08-03 11:49:44 +0530

    updat

--- pinak.java
+++ pinak.java
@@ -1,7 +1,8 @@
 import java.util.*;
 
-public class Main {
+public class Pinak {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Updte 2");
     }
 }
